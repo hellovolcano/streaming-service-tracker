ef5b97a90ea1d676106e8b7847ac39b1bd099541
Kate Lam <nhatthao_lam@outlook.com>
2022-08-12 17:51:09 -0500

    login routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,13 @@
-const router = require('express').Router()
-const sequelize = require('../config/connection')
+const router = require('express').Router();
+const sequelize = require('../config/connection');
 
 // GET all posts
 router.get('/', (req, res) => {
     res.render('home')
-})
+});
 
+router.get('/login', (req, res) => {
+    res.render('login')
+});
 
-module.exports = router
+module.exports = router;
