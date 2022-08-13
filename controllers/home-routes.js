eb6808e4c7bfff358de606807cc7906f21f09b36
Kate Lam <nhatthao_lam@outlook.com>
2022-08-13 15:45:36 -0500

    implemented signup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -45,20 +45,20 @@ router.post('/login', async (req, res) => {
     }
 });
 router.delete('/login', (req, res) => {
-  if (req.session) {
-    req.session.destroy(err => {
-      if (err) {
-        res.render('error', {
-            error: 'Unable to log out'
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.render('error', {
+                    error: 'Unable to log out'
+                });
+            } else {
+                // Redirect to home page
+                res.redirect('/');
+            }
         });
-      } else {
-        // Redirect to home page
-        res.redirect('/');
-      }
-    });
-  } else {
-    res.end()
-  }
+    } else {
+        res.end()
+    }
 });
 
 // GET all services
@@ -72,6 +72,54 @@ router.get('/services', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+router.post('/signup', async (req, res) => {
+
+    // Capture the input fields
+    const userData = {
+        password: req.body.password,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    }
+    const confirmPassword = req.body.confirmPassword;
+    let errMsg = '';
+    const emptyFields = [];
+    // Ensure the input fields exists and are not empty
+    Object.keys(userData).forEach((key) => {
+        if (!userData[key]) {
+            emptyFields.push(key);
+        }
+    });
+    if (emptyFields.length) {
+        errMsg = 'Please enter values for all required fields';
+        console.log(emptyFields);
+    } else {
+        if (userData.password !== confirmPassword) {
+            errMsg = 'The password and confirmation password do not match.';
+        }
+        
+        let user = await User.findOne({
+            where: {
+                email: userData.email
+            }
+        });
+        if (user) {
+            errMsg = 'Email already exists, please enter a different email address.';
+        }
+    }
+
+    if (!errMsg) {
+        user = User.build(userData);
+        await user.save();
+        res.redirect('/login?msg=user_created');
+    } else {
+        res.render('signup',{error:errMsg});
+    }
 });
 
 module.exports = router;
