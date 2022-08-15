a1ae6089f1139d496cea2d89445a21c5b1d6dbef
hellovolcano <valerie.gleason@gmail.com>
2022-08-15 10:06:57 -0500

    dashboard mockup to show create/edit actions for the service in action

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,17 +3,25 @@ const sequelize = require('../config/connection');
 const { Service, User } = require('../models');
 
 // render the user dashboard
+// TODO (BLOCKED): Filter the list of returned services to the specific user after we implement the junction table
 router.get('/', (req,res) => {
-    res.render('dashboard')
+    Service.findAll()
+        .then(serviceData => {
+            const services = serviceData.map(service => service.get({ plain: true }))
+        res.render('dashboard', {services})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
-
 // create a new service
 router.get('/create-service', (req,res) => {
     res.render('create-service')
 })
 
 // edit a service
-router.get('/edit/:id', (req,res) => {
+router.get('/edit-service/:id', (req,res) => {
     Service.findOne({
         where: {
             id: req.params.id
